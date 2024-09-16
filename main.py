676183101b8c02329d96858433bdf94eb7f76dc4
elmvel <goodd7183+2@gmail.com>
2024-09-16 18:00:01 -0400

    Expand the skeleton to provide the basic structure

--- main.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    quit = False
-    while not quit:
-        command = input('Enter UML Command: ')
-        match command:
-            case 'temp':
-                print('Temporary test!')
-            case _:
-                print('error! print some help here')
