2693ac41c19a289e7d29f66318b7826df1c3bba7
elmvel <goodd7183+2@gmail.com>
2024-09-18 16:13:36 -0400

    Implement relationshipAdd function

--- src/editor.py
+++ src/editor.py
@@ -1,5 +1,6 @@
 class Editor:
     classes = {}
+    relationships = set()
     def classAdd(self, name):
         pass
     def classDelete(self, name):
@@ -10,3 +11,17 @@ class Editor:
             print(f'ERROR: No class exists with the name `{name}`')
     def classRename(self, name, rename):
         pass
+
+    # Function which adds a relationship between class1 and class2, which are both strings
+    def relationshipAdd(self, class1, class2):
+        # We use tuples to make it simple to check for relationship existence in both orders
+        if (class1, class2) in self.relationships or (class2, class1) in self.relationships:
+            print(f'ERROR: There is already a relationship between `{class1}` and `{class2}`')
+        elif class1 not in self.classes:
+            print(f'ERROR: class `{class1}` does not exist')
+        elif class2 not in self.classes:
+            print(f'ERROR: class `{class2}` does not exist')
+        else:
+            # Note the extra parenthesis as we are adding a tuple to the set
+            self.relationships.add((class1, class2))
+            print(f'Added relationship between {class1} and {class2}!')
