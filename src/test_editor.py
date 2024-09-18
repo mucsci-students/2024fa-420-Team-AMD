4d95ea3eef5cf876f0ceff7d65fac5d239f93be5
Mia Joseph <miashiojos@gmail.com>
2024-09-18 19:18:48 -0400

    Update test_editor.py

--- src/test_editor.py
+++ src/test_editor.py
@@ -15,6 +15,25 @@ class testEditor(unittest.TestCase):
         editor.classes['Foo'] = Class()
         editor.classDelete('Bar')
         assert 'Foo' in editor.classes, 'Foo did not remain after failed deletion'
+
+    def testClassRenameSuccess(self):
+        editor = Editor()
+        editor.classes['Foo'] = Class()
+        editor.classRename('Foo', 'Bar')
+        assert 'Bar' in editor.classes and 'Foo' not in editor.classes, 'Foo was not renamed to Bar'
+    
+    def testClassRenameFailure1(self):
+        editor = Editor()
+        editor.classes['Foo'] = Class()
+        result = editor.classRename('Bar', 'Foo2')
+        assert result == False, 'Bar does not exist, rename failed'
+    
+    def testClassREnameFailure2(self):
+        editor = Editor()
+        editor.classes['Foo'] = Class()
+        editor.classes['Bar'] = Class()
+        result = editor.classRename('Foo', 'Bar')
+        assert result == False, 'Foo could not be renamed Bar: Bar already exists.'
         
 if __name__ == '__main__':
     unittest.main()
