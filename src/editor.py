59dcb0ea1f362869ce18877d78481d8cb4929111
elmvel <goodd7183+2@gmail.com>
2024-09-22 16:27:44 -0400

    Fix printing and semantics

--- src/editor.py
+++ src/editor.py
@@ -3,12 +3,12 @@ class Editor:
     classes = {}
     # Checks if a class of the given name already exists, if not, a new one is created
     def classAdd(self, name):
-        for item in classes:
-            if item.name == name:
-                return "This class already exists"
-        newclass = Class(name)
-        classes.add(newclass)
-        return "class added"
+        if name in self.classes:
+            print(f'Class {name} already exists')
+        else:
+            newclass = Class(name)
+            self.classes[name] = newclass
+            print(f'Added class {name}!')
         
     def classDelete(self, name):
         pass
