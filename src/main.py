075d288176da0e44b5e304eecb38b3cbc2cead8e
Mia Joseph <miashiojos@gmail.com>
2024-09-22 00:19:52 -0400

    exit

--- src/main.py
+++ src/main.py
@@ -76,6 +76,7 @@ if __name__ == '__main__':
             case 'help':
                 pass
             case 'exit':
-                pass
+                quit = True
+                break
             case _:
                 print('error! print some help here')
