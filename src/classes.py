8eba19e325d57b2f5504c201f6fe7bc2c9b0c15f
elmvel <goodd7183+2@gmail.com>
2024-09-22 17:57:10 -0400

    Implement saving of editor to JSON file

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,5 @@
 class Class:
+    # Creates a new class object with paremeters name and an empty set list for attributes#
     def __init__(self, name):
         self.name = name
         self.attributtesSets = set()
