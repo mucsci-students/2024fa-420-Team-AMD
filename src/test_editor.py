9460f1bfb79219ee6aa26f998aceb751dcf88348
RedApple03 <109757575+MrFrost9v@users.noreply.github.com>
2024-09-22 20:37:34 -0400

    added test for attribute methods
    created success/failure tests for the attribute methods.

--- src/test_editor.py
+++ src/test_editor.py
@@ -4,6 +4,7 @@ from editor import *
 from classes import *
 
 class testEditor(unittest.TestCase):
+    
     def testClassDeleteSuccess(self):
         editor = Editor()
         editor.classes['Foo'] = Class()
@@ -90,5 +91,60 @@ class testEditor(unittest.TestCase):
         editor.relationshipDelete('Foo', 'Baz')
         assert ('Foo', 'Bar') in editor.relationships and ('Foo', 'Baz') not in editor.relationships, 'Relationship should not have been removed'
 
+    def testAddAttributeSuccess(self):
+        editor = Editor()
+        editor.classAdd('Foo')
+
+        editor.addAttribute('Foo', 'at')
+        assert 'at' in editor.classes['Foo'].attributtesSets, 'Attribute added to class'
+
+    # Failure due to class not existing
+    def testAddAttributeFailure1(self):
+        editor = Editor()
+
+        editor.addAttribute('Foo', 'at')
+        assert 'Foo' not in editor.classes, 'class was not checked for existence'
+    
+    # Failure due to attribute already existing in class
+    def testAddAttributeFailure2(self):
+        editor = Editor()
+        editor.classAdd('Foo')
+        editor.addAttribute('Foo', 'at')
+
+        editor.addAttribute('Foo', 'at')
+        assert 'at' in editor.classes['Foo'].attributtesSets, 'duplication was not checked for'
+
+    def testRenameAttributeSuccess(self):
+        editor = Editor()
+        editor.classAdd('Foo')
+        editor.addAttribute('Foo', 'at')
+
+        editor.renameAttribute('Foo', 'at', 'at2')
+        assert 'at2' in editor.classes['Foo'].attributtesSets and 'at' not in editor.classes['Foo'].attributtesSets, 'Attribute name was not changed'
+
+    # Failure due to class not existing
+    def testRenameAttributeFailure1(self):
+        editor = Editor()
+
+        editor.renameAttribute('Foo', 'at', 'at2')
+        assert 'Foo' not in editor.classes, 'class not checked for existence'
+    
+    # Faiure due to original attribute not existing
+    def testRenameAttributeFailure2(self):
+        editor = Editor()
+        editor.classAdd('Foo')
+
+        editor.renameAttribute('Foo', 'at', 'at2')
+        assert 'at2' not in editor.classes['Foo'].attributtesSets and 'at' not in editor.classes['Foo'].attributtesSets, 'Attribute not checked for existence'
+    
+    # Failure due to new attrubute name already existing
+    def testRenameAttributeFailure3(self):
+        editor = Editor()
+        editor.classAdd('Foo')
+        editor.addAttribute('Foo', 'at')
+        editor.addAttribute('Foo', 'at2')
+
+        editor.renameAttribute('Foo', 'at', 'at2')
+        assert 'at2' in editor.classes['Foo'].attributtesSets and 'at' in editor.classes['Foo'].attributtesSets, 'Duplication was not checked for'
 if __name__ == '__main__':
     unittest.main()
