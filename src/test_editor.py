5f1e161483a5e6ed3a114736359a66ade7d10b76
elmvel <goodd7183+2@gmail.com>
2024-09-23 12:59:38 -0400

    Fix tests to construct Class object correctly

--- src/test_editor.py
+++ src/test_editor.py
@@ -7,41 +7,41 @@ class testEditor(unittest.TestCase):
     
     def testClassDeleteSuccess(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
+        editor.classes['Foo'] = Class('Foo')
         editor.classDelete('Foo')
         assert editor.classes == {}, 'Foo was not deleted'
 
     def testClassDeleteFailure(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
+        editor.classes['Foo'] = Class('Foo')
         editor.classDelete('Bar')
         assert 'Foo' in editor.classes, 'Foo did not remain after failed deletion'
         
     def testClassRenameSuccess(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
+        editor.classes['Foo'] = Class('Foo')
         editor.classRename('Foo', 'Bar')
         assert 'Bar' in editor.classes and 'Foo' not in editor.classes, 'Foo was not renamed to Bar'
     
     # Failure due to targeted class of renaming not existing
     def testClassRenameFailure1(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
+        editor.classes['Foo'] = Class('Foo')
         editor.classRename('Bar', 'Foo2')
         assert 'Foo2' not in editor.classes and 'Bar' not in editor.classes, 'Bar does not exist, rename failed'
     
     # Failure due to class of new name already existing
     def testClassRenameFailure2(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
         editor.classRename('Foo', 'Bar')
         assert 'Foo' in editor.classes and 'Bar' in editor.classes, 'Foo could not be renamed Bar: Bar already exists.'
 
     def testRelationshipAddSuccess(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
         editor.relationshipAdd('Foo', 'Bar')
         # relationshipAdd will always, on success, insert a tuple with the order of the arguments given
         assert ('Foo', 'Bar') in editor.relationships, 'Relationship was not added successfully'
@@ -49,8 +49,8 @@ class testEditor(unittest.TestCase):
     # Failure due to already existing relationship
     def testRelationshipAddFailure1(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
         editor.relationshipAdd('Foo', 'Bar')
 
         editor.relationshipAdd('Bar', 'Foo')
@@ -59,14 +59,14 @@ class testEditor(unittest.TestCase):
     # Failure due to missing class
     def testRelationshipAddFailure2(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
+        editor.classes['Foo'] = Class('Foo')
         editor.relationshipAdd('Foo', 'Bar')
         assert ('Foo', 'Bar') not in editor.relationships, 'Class was not checked for existence'
 
     def testRelationshipDeleteSuccess(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
         editor.relationships.add(('Foo', 'Bar'))
 
         editor.relationshipDelete('Foo', 'Bar')
@@ -75,8 +75,8 @@ class testEditor(unittest.TestCase):
     # Failure due to no relationship
     def testRelationshipDeleteFailure1(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
 
         editor.relationshipDelete('Foo', 'Bar')
         assert ('Foo', 'Bar') not in editor.relationships, 'Relationship should not have been removed'
@@ -84,8 +84,8 @@ class testEditor(unittest.TestCase):
     # Failure due to missing classes
     def testRelationshipDeleteFailure2(self):
         editor = Editor()
-        editor.classes['Foo'] = Class()
-        editor.classes['Bar'] = Class()
+        editor.classes['Foo'] = Class('Foo')
+        editor.classes['Bar'] = Class('Bar')
         editor.relationships.add(('Foo', 'Bar'))
 
         editor.relationshipDelete('Foo', 'Baz')
